d1f20f01da66fe675afcfc650d9ea1dfad692f4a
Blagovest Georgiev <47055593+Behemovt@users.noreply.github.com>
2020-04-02 13:43:55 +0300

    Create solve.js

---
+++ 4. Order the names/solve.js
@@ -0,0 +1,15 @@
+function solve() {
+    let button = document.querySelector('button');
+    let listPosition = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's' , 't', 'u', 'v', 'w', 'z', 'y', 'z'];
+    
+    let handler = e => {
+        let currentInput = document.querySelectorAll('input')[0];
+        let firstLetter = currentInput.value[0].toLocaleLowerCase();
+        let firstLetterIndex = listPosition.indexOf(firstLetter);
+        let inputPosition  = document.querySelectorAll('li')[firstLetterIndex];
+        inputPosition.innerHTML = currentInput.value;
+        currentInput.value = '';
+    }
+    
+    button.addEventListener('click', handler);
+}
